56497b5eb6ebf2215ac7ff27b6e162828e6834f1
Prathmesh <prathmeshparteki@gmail.com>
2026-02-17 00:04:17 +0530

    Initial Commit

---
+++ WiseMoney/WiseMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiseMoneyApp.swift
+//  WiseMoney
+//
+//  Created by Prathmesh Parteki on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct WiseMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
